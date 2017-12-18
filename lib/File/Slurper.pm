bdf05e4c04d894635b84b58b71713e020a8a0888
Leon Timmermans <fawaka@gmail.com>
2017-12-18 20:46:59 +0100

    Add STOP_AT_PARTIAL to $PerlIO::encoding::fallback
    Otherwise, characters overlapping buffer boundaries would cause failues

--- lib/File/Slurper.pm
+++ lib/File/Slurper.pm
@@ -6,7 +6,7 @@ use warnings;
 use Carp 'croak';
 use Exporter 5.57 'import';
 
-use Encode qw(:fallbacks);
+use Encode qw/FB_CROAK STOP_AT_PARTIAL/;
 use PerlIO::encoding;
 
 our @EXPORT_OK = qw/read_binary read_text read_lines write_binary write_text read_dir/;
@@ -58,7 +58,7 @@ sub read_text {
 	my $layer = _text_layers($encoding, $crlf);
 	return read_binary($filename) if $layer eq ':raw';
 
-	local $PerlIO::encoding::fallback = FB_CROAK;
+	local $PerlIO::encoding::fallback = STOP_AT_PARTIAL | FB_CROAK;
 	open my $fh, "<$layer", $filename or croak "Couldn't open $filename: $!";
 	return do { local $/; <$fh> };
 }
@@ -68,7 +68,7 @@ sub write_text {
 	$encoding ||= 'utf-8';
 	my $layer = _text_layers($encoding, $crlf);
 
-	local $PerlIO::encoding::fallback = FB_CROAK;
+	local $PerlIO::encoding::fallback = STOP_AT_PARTIAL | FB_CROAK;
 	open my $fh, ">$layer", $filename or croak "Couldn't open $filename: $!";
 	print $fh $_[1] or croak "Couldn't write to $filename: $!";
 	close $fh or croak "Couldn't write to $filename: $!";
@@ -84,7 +84,7 @@ sub read_lines {
 	$encoding ||= 'utf-8';
 	my $layer = _text_layers($encoding, $crlf);
 
-	local $PerlIO::encoding::fallback = FB_CROAK;
+	local $PerlIO::encoding::fallback = STOP_AT_PARTIAL | FB_CROAK;
 	open my $fh, "<$layer", $filename or croak "Couldn't open $filename: $!";
 	return <$fh> if $skip_chomp;
 	my @buf = <$fh>;
